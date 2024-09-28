d427ddf72f86381cd702627b42fc89390c9a8a08
yxtan02 <yxtan02@gmail.com>
2024-09-28 15:25:03 +0800

    edit wordle

--- routes/wordle.py
+++ routes/wordle.py
@@ -7,25 +7,45 @@ from routes import app
 
 logger = logging.getLogger(__name__)
 
-WORD_LIST = ['slate', 'lucky', 'maser', 'gapes', 'wages', 'apple', 'brick', 'train', 'plate', 'crane']
-
-def get_next_guess(guess_history, evaluation_history):
-    possible_words = [word for word in WORD_LIST if word not in guess_history]
-    return possible_words[0] if possible_words else None
+confirmed = [''] * 5
+possible = {}
+letters = [True] * 26
 
 @app.route('/wordle-game', methods=['POST'])
-def evaluate():
+def wordle():
     data = request.get_json()
-    logging.info("Data sent for evaluation: {}".format(data))
+    guess_history = data["guessHistory"]
+    evaluation_history = data["evaluationHistory"]
 
-    guess_history = data.get("guessHistory", [])
-    evaluation_history = data.get("evaluationHistory", [])
+    n = len(guess_history)
+    for i in range(n):
+        g = guess_history[i]
+        e = evaluation_history[i]
+        for j in range(5):
+            if e[j] == 'O':
+                confirmed[j] = g[j]
+            elif e[j] == 'X':
+                if (possible.get(g[j]) == None):
+                    possible[g[j]] = [i for i in range(5) if i != j]
+                else:
+                    if j in possible[g[j]]:
+                        possible[g[j]] = possible[g[j]].remove(j)
+            elif e[j] == '-':
+                letters[ord(g[j]) - ord('a')] = False
 
-    next_guess = get_next_guess(guess_history, evaluation_history)
-    
-    if next_guess is None:
-        logging.error("No more possible words to guess.")
-        return json.dumps({"error": "No more possible words to guess"}), 400
-
-    logging.info("Next guess: {}".format(next_guess))
-    return json.dumps({"guess": next_guess})
+    guess = [''] * 5
+    for i in range(5):
+        if confirmed[i] != '':
+            guess[i] = confirmed[i]
+    for i in range(5):
+        if guess[i] == '':
+            for k in possible:
+                if i in possible[k]:
+                    guess[i] = k
+    for i in range(5):
+        if guess[i] == '':
+            for j in range(26):
+                if letters[j]:
+                    guess[i] = chr(j + ord('a'))
+                    break
+    return json.dumps({"guess": ''.join(guess)})
