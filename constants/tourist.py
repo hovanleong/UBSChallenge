58a30a3fefbae0a2922e83da47c7765c4a337653
rachtrx <rachmielteorenxiang@gmail.com>
2024-09-28 12:29:59 +0800

    Tourist v1

---
+++ constants/tourist.py
@@ -0,0 +1,107 @@
+TRAVELLING_TIME = {
+    "Tokyo Metro Ginza Line": 2,
+    "Tokyo Metro Marunouchi Line": 3,
+    "Tokyo Metro Hibiya Line": 2.5,
+    "Tokyo Metro Tozai Line": 4,
+    "Tokyo Metro Chiyoda Line": 1.5,
+    "Tokyo Metro Yurakucho Line": 2,
+    "Tokyo Metro Hanzomon Line": 2,
+    "Tokyo Metro Namboku Line" : 1,
+    "Tokyo Metro Fukutoshin Line": 3,
+    "Toei Asakusa Line": 3.5,
+    "Toei Mita Line": 4,
+    "Toei Shinjuku Line": 1.5,
+    "Toei Oedo Line": 1
+}
+
+TRAIN_LINES = {
+    "Tokyo Metro Ginza Line": [
+        "Asakusa", "Tawaramachi", "Inaricho", "Ueno", "Ueno-hirokoji", "Suehirocho",
+        "Kanda", "Mitsukoshimae", "Nihombashi", "Kyobashi", "Ginza", "Shimbashi",
+        "Toranomon", "Tameike-sanno", "Akasaka-mitsuke", "Nagatacho", "Aoyama-itchome",
+        "Gaiemmae", "Omotesando", "Shibuya"
+    ],
+    "Tokyo Metro Marunouchi Line": [
+        "Ogikubo", "Minami-asagaya", "Shin-koenji", "Higashi-koenji", "Shin-nakano",
+        "Nakano-sakaue", "Nishi-shinjuku", "Shinjuku", "Shinjuku-sanchome", "Shin-ochanomizu",
+        "Ochanomizu", "Awajicho", "Otemachi", "Tokyo", "Ginza", "Kasumigaseki", "Kokkai-gijidomae",
+        "Akasaka-mitsuke", "Yotsuya", "Yotsuya-sanchome", "Shinjuku-gyoemmae", "Nishi-shinjuku-gochome",
+        "Nakano-fujimicho", "Nakano-shimbashi", "Nakano-sakaue", "Shinjuku-sanchome", "Kokkai-gijidomae",
+        "Kasumigaseki", "Ginza", "Tokyo", "Otemachi", "Awajicho", "Shin-ochanomizu", "Ochanomizu"
+    ],
+    "Tokyo Metro Hibiya Line": [
+        "Naka-meguro", "Ebisu", "Hiroo", "Roppongi", "Kamiyacho", "Kasumigaseki", "Hibiya",
+        "Ginza", "Higashi-ginza", "Tsukiji", "Hatchobori", "Kayabacho", "Nihombashi",
+        "Kodemmacho", "Akihabara", "Naka-okachimachi", "Ueno", "Iriya", "Minowa",
+        "Minami-senju", "Kita-senju"
+    ],
+    "Tokyo Metro Tozai Line": [
+        "Nakano", "Ochiai", "Takadanobaba", "Waseda", "Kagurazaka", "Iidabashi", "Kudanshita",
+        "Takebashi", "Otemachi", "Nihombashi", "Kayabacho", "Monzen-nakacho", "Kiba",
+        "Toyosu", "Minami-sunamachi", "Nishi-kasai", "Kasai", "Urayasu", "Minami-gyotoku",
+        "Gyotoku", "Myoden", "Baraki-nakayama", "Nishi-funabashi"
+    ],
+    "Tokyo Metro Chiyoda Line": [
+        "Yoyogi-uehara", "Yoyogi-koen", "Meiji-jingumae", "Omotesando", "Nogizaka", "Akasaka",
+        "Kokkai-gijidomae", "Kasumigaseki", "Hibiya", "Nijubashimae", "Otemachi",
+        "Shin-ochanomizu", "Yushima", "Nezu", "Sendagi", "Nishi-nippori", "Machiya",
+        "Kita-senju", "Ayase", "Kita-ayase"
+    ],
+    "Tokyo Metro Yurakucho Line": [
+        "Wakoshi", "Chikatetsu-narimasu", "Chikatetsu-akatsuka", "Heiwadai", "Hikawadai",
+        "Kotake-mukaihara", "Senkawa", "Kanamecho", "Ikebukuro", "Higashi-ikebukuro",
+        "Gokokuji", "Edogawabashi", "Iidabashi", "Ichigaya", "Kojimachi", "Nagatacho",
+        "Sakuradamon", "Yurakucho", "Ginza-itchome", "Shintomicho", "Toyocho",
+        "Kiba", "Toyosu", "Tsukishima", "Shintomicho", "Tatsumi", "Shinonome", "Ariake"
+    ],
+    "Tokyo Metro Hanzomon Line": [
+        "Shibuya", "Omotesando", "Aoyama-itchome", "Nagatacho", "Hanzomon", "Kudanshita",
+        "Jimbocho", "Otemachi", "Mitsukoshimae", "Suitengumae", "Kiyosumi-shirakawa",
+        "Sumiyoshi", "Kinshicho", "Oshiage"
+    ],
+    "Tokyo Metro Namboku Line": [
+        "Meguro", "Shirokanedai", "Shirokane-takanawa", "Azabu-juban", "Roppongi-itchome",
+        "Tameike-sanno", "Nagatacho", "Yotsuya", "Ichigaya", "Iidabashi", "Korakuen",
+        "Todaimae", "Hon-komagome", "Komagome", "Nishigahara", "Oji", "Oji-kamiya",
+        "Shimo", "Akabane-iwabuchi"
+    ],
+    "Tokyo Metro Fukutoshin Line": [
+        "Wakoshi", "Chikatetsu-narimasu", "Chikatetsu-akatsuka", "Narimasu", "Shimo-akatsuka",
+        "Heiwadai", "Hikawadai", "Kotake-mukaihara", "Senkawa", "Kanamecho", "Ikebukuro",
+        "Zoshigaya", "Nishi-waseda", "Higashi-shinjuku", "Shinjuku-sanchome", "Kita-sando",
+        "Meiji-jingumae", "Shibuya"
+    ],
+    "Toei Asakusa Line": [
+        "Nishi-magome", "Magome", "Nakanobu", "Togoshi", "Gotanda", "Takanawadai",
+        "Sengakuji", "Mita", "Shiba-koen", "Daimon", "Shimbashi", "Higashi-ginza",
+        "Takaracho", "Nihombashi", "Ningyocho", "Higashi-nihombashi", "Asakusabashi",
+        "Kuramae", "Asakusa", "Honjo-azumabashi", "Oshiage"
+    ],
+    "Toei Mita Line": [
+        "Meguro", "Shirokanedai", "Shirokane-takanawa", "Mita", "Shiba-koen", "Onarimon",
+        "Uchisaiwaicho", "Hibiya", "Otemachi", "Jimbocho", "Suidobashi", "Kasuga",
+        "Hakusan", "Sengoku", "Sugamo", "Nishi-sugamo", "Shin-itabashi", "Itabashi-kuyakushomae",
+        "Itabashi-honcho", "Motohasunuma", "Shin-takashimadaira", "Nishidai", "Hasune",
+        "Takashimadaira", "Shimura-sakaue", "Shimura-sanchome", "Nishidai"
+    ],
+    "Toei Shinjuku Line": [
+        "Shinjuku", "Shinjuku-sanchome", "Akebonobashi", "Ichigaya", "Kudanshita",
+        "Jimbocho", "Ogawamachi", "Iwamotocho", "Bakuro-yokoyama", "Hamacho",
+        "Morishita", "Kikukawa", "Sumiyoshi", "Nishi-ojima", "Ojima", "Higashi-ojima",
+        "Funabori", "Ichinoe", "Mizue", "Shinozaki", "Motoyawata"
+    ],
+    "Toei Oedo Line": [
+        "Hikarigaoka", "Nerima-kasugacho", "Toshimaen", "Nerima", "Nerima-sakamachi",
+        "Shin-egota", "Ochiai-minami-nagasaki", "Nakai", "Higashi-nakano", "Nakano-sakaue",
+        "Nishi-shinjuku-gochome", "Tochomae", "Shinjuku-nishiguchi", "Higashi-shinjuku",
+        "Wakamatsu-kawada", "Ushigome-yanagicho", "Ushigome-kagurazaka", "Iidabashi",
+        "Kasuga", "Hongosanchome", "Ueno-okachimachi", "Shin-okachimachi", "Kuramae",
+        "Ryogoku", "Morishita", "Kiyosumi-shirakawa", "Monzen-nakacho", "Tsukishima",
+        "Kachidoki", "Shiodome", "Daimon", "Akasaka-mitsuke", "Roppongi", "Aoyama-itchome",
+        "Shinjuku", "Tochomae", "Shinjuku", "Shinjuku-sanchome", "Higashi-shinjuku",
+        "Wakamatsu-kawada", "Ushigome-yanagicho", "Ushigome-kagurazaka", "Iidabashi",
+        "Kasuga", "Hongosanchome", "Ueno-okachimachi", "Shin-okachimachi", "Kuramae",
+        "Ryogoku", "Morishita", "Kiyosumi-shirakawa", "Monzen-nakacho", "Tsukishima",
+        "Kachidoki", "Shiodome", "Daimon", "Shiodome", "Tsukishima"
+    ]
+}
