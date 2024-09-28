62753eda3e8ae234f3d2b57e8fd94e186c8eb163
yxtan02 <yxtan02@gmail.com>
2024-09-28 16:04:34 +0800

    edit clumsy

--- routes/clumsy.py
+++ routes/clumsy.py
@@ -3,12 +3,11 @@ from flask import Flask, request, jsonify
 from routes import app
 from collections import defaultdict
 
-
-def differs_by_one(word1, word2):
-    return sum(1 for a, b in zip(word1, word2) if a != b) == 1
-
 @app.route('/the-clumsy-programmer', methods=['POST'])
 def clumsy():
+    def differs_by_one(word1, word2):
+        return sum(1 for a, b in zip(word1, word2) if a != b) == 1
+    
     data = request.get_json()
     results = []
 
