e2da8bdc2828ae45b88a575bd81ae1a9c7649254
yxtan02 <yxtan02@gmail.com>
2024-09-28 21:04:37 +0800

    edit wordle

--- routes/wordle.py
+++ routes/wordle.py
@@ -21,7 +21,7 @@ WORD_LIST = load_words_from_file(file_path)
 def filter_words(guess_history, evaluation_history):
     confirmed = [''] * 5
     possible = {}
-    letters = [1] * 26
+    letters = [5] * 26
     n = len(guess_history)
     for i in range(n):
         g = guess_history[i]
@@ -29,16 +29,14 @@ def filter_words(guess_history, evaluation_history):
         for j in range(5):
             if e[j] == 'O':
                 confirmed[j] = g[j]
-                letters[ord(g[j]) - ord('a')] += 1
             elif e[j] == 'X':
                 if possible.get(g[j]) == None:
                     possible[g[j]] = [i for i in range(5) if i != j]
                 else:
                     if j in possible[g[j]]:
                         possible[g[j]].remove(j)  
-                letters[ord(g[j]) - ord('a')] += 1                      
             elif e[j] == '-':    
-                letters[ord(g[j]) - ord('a')] -= 1
+                letters[ord(g[j]) - ord('a')] = 0
            
 
     res = ''
